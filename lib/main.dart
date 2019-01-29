9727ad2420a972a58d2d2464fb472d7f67cd9b16
Renato Ferraz <rfcbf@me.com>
2019-01-29 11:36:07 -0300

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ void main() {
   runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      accentColor: Colors.amber,
+      hintColor: Colors.amber,
+      primaryColor: Colors.amberAccent,
+    ),
   ));
 }
 
@@ -34,8 +39,6 @@ class _HomeState extends State<Home> {
     setState(() {
       _getData();
     });
-
-
   }
 
   void _excluirTodo(String id) async {
@@ -63,90 +66,183 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Lista de Tarefas"),
-          backgroundColor: Colors.blueAccent,
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text(
+          "Lista de Tarefas",
+          style: TextStyle(color: Colors.white),
         ),
-        body: buildTela());
+        backgroundColor: Colors.amber[400],
+        centerTitle: false,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.error,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              _showDialogInfo(context);
+            },
+          ),
+        ],
+      ),
+      body: buildTela(context),
+    );
   }
 
-  Widget buildTela() {
-    return Column(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                  child: TextField(
-                controller: _toDoController,
-                decoration: InputDecoration(
-                    labelText: "Nova Tarefa",
-                    labelStyle: TextStyle(color: Colors.blueAccent)),
-              )),
-              RaisedButton(
-                color: Colors.blueAccent,
-                child: Text("Adicionar"),
-                textColor: Colors.white,
-                onPressed: _addToDo,
-              )
-            ],
-          ),
-        ),
-        Expanded(
-          child: RefreshIndicator(
-            onRefresh: _refresh,
-            child: FutureBuilder(
-              future: _getData(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) print(snapshot.error);
+  Widget buildTela(BuildContext context) {
+    return Container(
+//      color: Colors.amber[100],
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: FutureBuilder(
+                future: _getData(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) print(snapshot.error);
 
-                return snapshot.hasData
-                    ? buildList() //buildList(snapshot.data)
-                    : Center(child: CircularProgressIndicator());
-              },
+                  return snapshot.hasData
+                      ? buildList() //buildList(snapshot.data)
+                      : Center(child: CircularProgressIndicator());
+                },
+              ),
             ),
           ),
-        ),
-      ],
+          Container(
+//            color: Colors.amber[100],
+            padding: EdgeInsetsDirectional.only(bottom: 20.0),
+            child: FloatingActionButton(
+                elevation: 4.0,
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+                backgroundColor: Colors.amber[400],
+                onPressed: () {
+                  _showDialog(context);
+                }),
+          ),
+        ],
+      ),
     );
   }
 
   Widget buildList() {
-    return ListView.builder(
-        padding: EdgeInsets.only(top: 10.0),
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return Dismissible(
-            key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-            background: Container(
-              color: Colors.red,
-              child: Align(
-                alignment: Alignment(-0.9, 0.0),
-                child: Icon(
-                  Icons.delete,
-                  color: Colors.white,
+    return Container(
+//      color: Colors.amber[100],
+      child: ListView.builder(
+          padding: EdgeInsets.only(top: 10.0),
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return Dismissible(
+              key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+              background: Container(
+                color: Colors.red,
+                child: Align(
+                  alignment: Alignment(-0.9, 0.0),
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            direction: DismissDirection.startToEnd,
-            child: CheckboxListTile(
-              title: Text(todos[index].texto),
-              value: todos[index].concluido,
-              secondary: CircleAvatar(
-                child: Icon(todos[index].concluido ? Icons.check : Icons.error),
+              direction: DismissDirection.startToEnd,
+              child: CheckboxListTile(
+                activeColor: Colors.green,
+                title: Text(todos[index].texto),
+                value: todos[index].concluido,
+                secondary: todos[index].concluido
+                    ? CircleAvatar(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.green,
+                        child: Icon(Icons.check),
+                      )
+                    : CircleAvatar(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.redAccent,
+                        child: Icon(Icons.error),
+                      ),
+                onChanged: (c) {
+                  todos[index].concluido = c;
+                  _atualizaStatus(todos[index].id.toString());
+                },
               ),
-              onChanged: (c) {
-                todos[index].concluido = c;
-                _atualizaStatus(todos[index].id.toString());
+              onDismissed: (direction) {
+                _excluirTodo(todos[index].id.toString());
+                todos.removeAt(index);
               },
-            ),
-            onDismissed: (direction) {
-              _excluirTodo(todos[index].id.toString());
-              todos.removeAt(index);
-            },
-          );
-        });
+            );
+          }),
+    );
+  }
+
+  Future<void> _showDialog(BuildContext context) async {
+    return await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: new Row(
+            children: <Widget>[
+              new Expanded(
+                child: new TextField(
+                  controller: _toDoController,
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                    border: new OutlineInputBorder(
+                        borderSide: new BorderSide(color: Colors.amber)),
+                    fillColor: Colors.amber,
+                    labelStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.amber,
+                        decorationColor: Colors.amber),
+                    labelText: 'Nova Tarefa:',
+                  ),
+                ),
+              )
+            ],
+          ),
+          actions: <Widget>[
+            new FlatButton(
+                child: const Text('Cancelar'),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+            new FlatButton(
+                child: const Text('Adicionar'),
+                onPressed: () {
+                  if (_toDoController != null && _toDoController.text != '' && _toDoController.text != null){
+                    _addToDo();
+                    Navigator.pop(context);
+                  }
+                }),
+          ],
+        );
+      },
+    );
+  }
+
+  _showDialogInfo(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Sobre',
+          ),
+          content: Container(
+            child: new Text(
+                'Desenvolvido por: Renato Ferraz\nVersão: 2.0\n\nDesafio da ESIG'),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+                child: const Text('Fechar'),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+          ],
+        );
+      },
+    );
   }
 }
